138999618795945181d0956fbfbca930dc730d22
IsMit0 <mitchell.ij@oundleschool.co.uk>
2026-09-17 10:02:00 +0100

    testpush2

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,3 @@
-import { createClient } from '@/utils/supabase/server'
-import { cookies } from 'next/headers'
-
-export default async function Page() {
-  const cookieStore = await cookies()
-  const supabase = createClient(cookieStore)
-
-  const { data: instruments } = await supabase.from('instruments').select()
-
-  return (
-
-    <ul>
-      {instruments?.map((instruments) => (
-        <li key={instruments.id}>{instruments.name}</li>
-      ))}
-    </ul>
-  )
+export default function Page() {
+  return <h1>Hello Next.js!</h1>
 }
