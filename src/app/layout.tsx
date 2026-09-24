c52faf7b14d641bd1a8cd306dc6fa9d1c68fd51e
IsMit0 <mitchell.ij@oundleschool.co.uk>
2026-09-24 09:59:47 +0100

    LETSGETTHISSHOWONTHEROAD

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -29,7 +29,11 @@ export default function RootLayout({
         <ClerkProvider>
           <header className="flex justify-end items-center p-4 gap-4 h-16">
             <Show when="signed-out">
-              <SignInButton />
+              <SignInButton>
+                <button className="bg-purple-700 text-white rounded-full font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 cursor-pointer">
+                  Sign in
+                </button>
+              </SignInButton>
               <SignUpButton>
                 <button className="bg-purple-700 text-white rounded-full font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 cursor-pointer">
                   Sign Up
